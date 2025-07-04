c574402429b2267e773cdd1974b5cb7b1ca6446f
Olivier Marteaux <olivier_marteaux@hotmail.com>
2025-07-04 19:14:22 +0200

    corrected accessibility on screens 01 and 02

--- app/src/main/java/fr/opc/practice/p9a11y/Case2Activity.kt
+++ app/src/main/java/fr/opc/practice/p9a11y/Case2Activity.kt
@@ -3,6 +3,7 @@ package fr.opc.practice.p9a11y
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
 import fr.opc.practice.p9a11y.databinding.ActivityCase2Binding
 
 class Case2Activity : AppCompatActivity() {
@@ -21,9 +22,37 @@ class Case2Activity : AppCompatActivity() {
             setFavouriteButtonIcon(isFavourite)
         }
 
+        ViewCompat.addAccessibilityAction(
+            // View to add accessibility action
+            binding.recipeCard,
+            // Label surfaced to user by an accessibility service
+            getText(R.string.cd_ajouter_aux_favoris),
+        ) { _, _ ->
+            // Same method executed when swiping on itemView
+            isFavourite = !isFavourite
+            setFavouriteButtonIcon(isFavourite)
+            if (isFavourite) {
+                binding.recipeCard.announceForAccessibility(getString(R.string.recette_ajout_e_aux_favoris))
+            } else {binding.recipeCard.announceForAccessibility(getString(R.string.recette_supprim_e_des_favoris))}
+            true
+        }
+
         binding.addRecipeToBasket.setOnClickListener {
             Toast.makeText(this, getString(R.string.recette_ajout_au_panier), Toast.LENGTH_SHORT)
                 .show()
+        }
+
+        ViewCompat.addAccessibilityAction(
+            // View to add accessibility action
+            binding.recipeCard,
+            // Label surfaced to user by an accessibility service
+            getText(R.string.ajouter_la_recette_au_panier),
+        ){
+            _, _ ->
+            // Same method executed when swiping on itemView
+            Toast.makeText(this, getString(R.string.recette_ajout_au_panier), Toast.LENGTH_SHORT)
+                .show()
+            true
         }
 
         binding.recipeCard.setOnClickListener {
